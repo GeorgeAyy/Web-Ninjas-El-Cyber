d0ad43fa1eb8bf60736d726b1feb012267a91686
George Aziz <123029529+GeorgeAyy@users.noreply.github.com>
2023-03-11 22:27:16 +0200

    Added slideshow and images with edits to html css and javascript

---
+++ Project/JS/handler.js
@@ -0,0 +1,21 @@
+// ##################  START SLIDE SHOW SCRIPT ######################
+var counter = 1;
+var imageSlide = document.getElementsByClassName("image");
+var buttons = document.getElementsByClassName("manual-btn");
+function displayImage(num) {
+  for (var i = 0; i < imageSlide.length; i++) {
+    if (i == num) {
+      imageSlide[i].style.display = "block";
+      buttons[i].style.background = "black";
+    } else {
+      imageSlide[i].style.display = "none";
+      buttons[i].style.background = "transparent";
+    }
+  }
+}
+function displayFirst() {
+  imageSlide[0].style.display = "block";
+  buttons[0].style.background = "black";
+}
+displayFirst();
+// ##################  END SLIDE SHOW SCRIPT ######################
