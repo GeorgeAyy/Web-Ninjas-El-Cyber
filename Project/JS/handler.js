ec87226bbbee472affb8930fa05519f0ebcc36ca
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2023-03-23 17:20:47 +0200

    updates.

--- Project/JS/handler.js
+++ Project/JS/handler.js
@@ -239,3 +239,13 @@ function removeItem() {
     ).toLocaleString();
   }
 }
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
