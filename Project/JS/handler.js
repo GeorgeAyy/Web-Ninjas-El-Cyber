2964a6421fa74d689e3780379f2ae2098c0285b0
George Aziz <123029529+GeorgeAyy@users.noreply.github.com>
2023-04-03 16:49:24 +0200

    Made the slideshow automatic

--- Project/JS/handler.js
+++ Project/JS/handler.js
@@ -1,6 +1,6 @@
 // ##################  START SLIDE SHOW SCRIPT ######################
 var counter = 1;
-var imageSlide = document.getElementsByClassName("image");
+var imageSlide = document.getElementsByClassName("slideimage");
 var buttons = document.getElementsByClassName("manual-btn");
 function displayImage(num) {
   for (var i = 0; i < imageSlide.length; i++) {
@@ -18,6 +18,35 @@ function displayFirst() {
   buttons[0].style.background = "black";
 }
 displayFirst();
+function nextSlide() {
+  for (var i = 0; i < imageSlide.length; i++) {
+    console.log("first log: " + i);
+    if (imageSlide[i].style.display == "block") {
+      if (i === (imageSlide.length - 1)) {
+        console.log(i);
+        imageSlide[i].style.display = "none";
+        buttons[i].style.background = "transparent";
+        imageSlide[0].style.display = "block";
+        buttons[0].style.background = "black";
+      } else {
+        imageSlide[i].style.display = "none";
+        buttons[i].style.background = "transparent";
+        imageSlide[i+1].style.display = "block";
+        buttons[i+1].style.background = "black";
+      }
+      break;
+    }
+  }
+}
+function automaticImage(){
+  setInterval(function () {
+    nextSlide();
+  }, 8000);
+}
+
+window.onload = function() {
+  automaticImage();  
+  }
 // ##################  END SLIDE SHOW SCRIPT ######################
 
 function signOpen() {
@@ -36,176 +65,149 @@ function cartClose() {
   document.getElementById("cartsidebar").style.display = "none";
 }
 
-
-function validateFirstName(field){
-  if(field==''){
-    document.getElementById('first').innerHTML='You must enter your first name !';
-    document.getElementById('fir').style.borderColor='red';
+function validateFirstName(field) {
+  if (field == "") {
+    document.getElementById("first").innerHTML =
+      "You must enter your first name !";
+    document.getElementById("fir").style.borderColor = "red";
     return false;
-  }
-  else{
-    document.getElementById('first').innerHTML='';
-    document.getElementById('fir').style.borderColor='black';
+  } else {
+    document.getElementById("first").innerHTML = "";
+    document.getElementById("fir").style.borderColor = "black";
     return true;
   }
 }
 
-function validateLastName(field){
-  if(field==''){
-    document.getElementById('last').innerHTML='You must enter your Last name !';
-    document.getElementById('las').style.borderColor='red';
+function validateLastName(field) {
+  if (field == "") {
+    document.getElementById("last").innerHTML =
+      "You must enter your Last name !";
+    document.getElementById("las").style.borderColor = "red";
     return false;
-  }
-  else{
-    document.getElementById('last').innerHTML='';
-    document.getElementById('las').style.borderColor='black';
+  } else {
+    document.getElementById("last").innerHTML = "";
+    document.getElementById("las").style.borderColor = "black";
     return true;
   }
 }
 
-function validatePassword(field1, field2){
-  let valid=true;
-  if(field1==''){
-    document.getElementById('pas').innerHTML='You must enter a password !';
-    document.getElementById('pa').style.borderColor='red'; 
-    document.getElementById('pac').style.borderColor='red';
+function validatePassword(field1, field2) {
+  let valid = true;
+  if (field1 == "") {
+    document.getElementById("pas").innerHTML = "You must enter a password !";
+    document.getElementById("pa").style.borderColor = "red";
+    document.getElementById("pac").style.borderColor = "red";
 
-    valid=false;
-    
-    
+    valid = false;
+  } else if (field1.length != 8) {
+    document.getElementById("pas").innerHTML =
+      "Password must be at least 8 characters !";
+    document.getElementById("pa").style.borderColor = "red";
+  } else {
+    document.getElementById("pas").innerHTML = "";
+    document.getElementById("pa").style.borderColor = "black";
   }
-  else if(field1.length!=8){
-    document.getElementById('pas').innerHTML='Password must be at least 8 characters !';
-    document.getElementById('pa').style.borderColor='red'; 
-  }
-  else{
-    document.getElementById('pas').innerHTML='';
-    document.getElementById('pa').style.borderColor='black'; 
-
-  }
-  if(field1!=field2){
-    document.getElementById('con').innerHTML='Password does not match !';
-    document.getElementById('pac').style.borderColor='red';
-    valid=false;
-  }
-  else{
-    document.getElementById('con').innerHTML='';
-    document.getElementById('pac').style.borderColor='black';
-
+  if (field1 != field2) {
+    document.getElementById("con").innerHTML = "Password does not match !";
+    document.getElementById("pac").style.borderColor = "red";
+    valid = false;
+  } else {
+    document.getElementById("con").innerHTML = "";
+    document.getElementById("pac").style.borderColor = "black";
   }
   return valid;
 }
 
 function validateEmail(email) {
-  if(email=='')
-  {
-    document.getElementById('em').innerHTML='You must enter your email !';
-    document.getElementById('ema').style.borderColor='red';
-    valid=false;
+  if (email == "") {
+    document.getElementById("em").innerHTML = "You must enter your email !";
+    document.getElementById("ema").style.borderColor = "red";
+    valid = false;
+  } else {
+    document.getElementById("em").innerHTML = "";
+    document.getElementById("ema").style.borderColor = "black";
   }
-  else
-  {
-    document.getElementById('em').innerHTML='';
-    document.getElementById('ema').style.borderColor='black';
-  }
- let regular_expressions = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let regular_expressions =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regular_expressions.test(String(email).toLocaleLowerCase());
 }
 
-function validatePassword1(field1 , field2){
-  let valid=true;
-  if(field1==''){
-    document.getElementById('passwor').innerHTML='You must enter a password !';
-    document.getElementById('pas1').style.borderColor='red'; 
-    valid=false;
-    
-    
-  }
-  else if(field1.length!=8){
-    document.getElementById('passwor').innerHTML='Password must be at least 8 characters !';
-    document.getElementById('pas1').style.borderColor='red'; 
-  }
-  else if((field1!="fyngz!.!" && field2=="fyngz@gmail.com")||(field1!="admin!.!" && field2=="admin@gmail.com")){
-    document.getElementById('passwor').innerHTML='Wrong password !';
-    document.getElementById('pas1').style.borderColor='red'; 
-  }
-  else if(field1=="admin!.!" && field2=="admin@gmail.com")
-  {
-    window.location.href="Admin.html";
-  }
-  else{
-    document.getElementById('passwor').innerHTML='';
-    document.getElementById('pas1').style.borderColor='black'; 
+function validatePassword1(field1, field2) {
+  let valid = true;
+  if (field1 == "") {
+    document.getElementById("passwor").innerHTML =
+      "You must enter a password !";
+    document.getElementById("pas1").style.borderColor = "red";
+    valid = false;
+  } else if (field1.length != 8) {
+    document.getElementById("passwor").innerHTML =
+      "Password must be at least 8 characters !";
+    document.getElementById("pas1").style.borderColor = "red";
+  } else if (
+    (field1 != "fyngz!.!" && field2 == "fyngz@gmail.com") ||
+    (field1 != "admin!.!" && field2 == "admin@gmail.com")
+  ) {
+    document.getElementById("passwor").innerHTML = "Wrong password !";
+    document.getElementById("pas1").style.borderColor = "red";
+  } else if (field1 == "admin!.!" && field2 == "admin@gmail.com") {
+    window.location.href = "Admin.html";
+  } else {
+    document.getElementById("passwor").innerHTML = "";
+    document.getElementById("pas1").style.borderColor = "black";
   }
   return valid;
 }
 
-
-function validate(form){
-  let fail='';
-  fail&=validateFirstName(form.Firstname.value.trim());
-  fail&=validateLastName(form.last.value.trim());
-  fail&=validateEmail(form.email.value.trim());
-  fail&=validatePassword(form.pas.value, form.pasconfirm.value);
-  if(fail)
-    return true;
-  else{
+function validate(form) {
+  let fail = "";
+  fail &= validateFirstName(form.Firstname.value.trim());
+  fail &= validateLastName(form.last.value.trim());
+  fail &= validateEmail(form.email.value.trim());
+  fail &= validatePassword(form.pas.value, form.pasconfirm.value);
+  if (fail) return true;
+  else {
     return false;
   }
 }
 
 function validateEmail1(email) {
   console.log(email);
-  if(email=='')
-  {
-    document.getElementById('em1').innerHTML='You must enter your email !';
-    document.getElementById('ema1').style.borderColor='red';
-    valid=false;
+  if (email == "") {
+    document.getElementById("em1").innerHTML = "You must enter your email !";
+    document.getElementById("ema1").style.borderColor = "red";
+    valid = false;
+  } else if (email != "fyngz@gmail.com" && email != "admin@gmail.com") {
+    document.getElementById("em1").innerHTML = "Not registered on this email !";
+    document.getElementById("ema1").style.borderColor = "red";
+  } else {
+    document.getElementById("em1").innerHTML = "";
+    document.getElementById("ema1").style.borderColor = "black";
   }
-  else if(email!="fyngz@gmail.com" && email!="admin@gmail.com"){
-    document.getElementById('em1').innerHTML='Not registered on this email !';
-    document.getElementById('ema1').style.borderColor='red'; 
-  }
-  else
-  {
-    document.getElementById('em1').innerHTML='';
-    document.getElementById('ema1').style.borderColor='black';
-  }
- let regular_expressions = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let regular_expressions =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regular_expressions.test(String(email).toLocaleLowerCase());
 }
 
-function openforgotsidebar()
-{
+function openforgotsidebar() {
   document.getElementById("forgotsidebar").style.display = "block";
   document.getElementById("sidebar-1").style.display = "none";
-
-  
-
 }
 
-function forgotclose()
-{
+function forgotclose() {
   document.getElementById("forgotsidebar").style.display = "none";
-
 }
 
-function backtosidebar()
-{
+function backtosidebar() {
   document.getElementById("forgotsidebar").style.display = "none";
   document.getElementById("sidebar-1").style.display = "block";
-
-
 }
 
-
-function validate1(form){
-  let fail='';
-  fail&=validateEmail1(form.email.value.trim());
-  fail&=validatePassword1(form.pas.value,form.email.value.trim());
-  if(fail)
-    return true;
-  else{
+function validate1(form) {
+  let fail = "";
+  fail &= validateEmail1(form.email.value.trim());
+  fail &= validatePassword1(form.pas.value, form.email.value.trim());
+  if (fail) return true;
+  else {
     return false;
   }
 }
@@ -232,7 +234,6 @@ function addItem() {
 function removeItem() {
   var x = parseInt(document.getElementById("totalAmount").innerText);
   if (x != 1) {
-    
     var y = parseFloat(
       document.getElementById("totalItemPrice").innerText.replace(/,/g, "")
     );
@@ -261,18 +262,15 @@ function addItemCart() {
   y /= x;
   x = x + 1;
   document.getElementById("totalAmountBar").innerText = x;
-  
 }
 function removeItemCart() {
   var x = parseInt(document.getElementById("totalAmountBar").innerText);
   if (x != 1) {
-   
     var y = parseFloat(
       document.getElementById("priceAfterSale").innerText.replace(/,/g, "")
     );
     y /= x;
     x--;
     document.getElementById("totalAmountBar").innerText = x;
-    
   }
 }
